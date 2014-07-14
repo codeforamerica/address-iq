150ef1f41bbaa35cc82fe9c02f535364cea31ad3
Dan Getelman <dgetelman@gmail.com>
2014-07-14 13:40:11 -0700

    Add basic SQLAlchemy importing in.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
