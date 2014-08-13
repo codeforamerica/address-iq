1fcba7fdb3c034326b6a4d2c3370a0e0d0ac4293
Fureigh <info@fureigh.com>
2014-08-12 18:21:43 -0700

    [75075362] Add login and logout config for BrowserID for testing.

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Config(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     BROWSERID_URL = os.environ['BROWSERID_URL']
+    BROWSERID_LOGIN_URL = '/log-in'
+    BROWSERID_LOGOUT_URL = '/log-out'
 
 class ProductionConfig(Config):
     DEBUG = False
