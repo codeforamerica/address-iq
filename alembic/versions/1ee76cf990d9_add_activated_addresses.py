8c98f4c8504bf0ce8d86f89b6c840d1b904e04f2
Dan Getelman <dgetelman@gmail.com>
2014-09-04 12:00:26 -0700

    Update alembic to be in a sane order

--- alembic/versions/1ee76cf990d9_add_activated_addresses.py
+++ alembic/versions/1ee76cf990d9_add_activated_addresses.py
@@ -1,14 +1,14 @@
 """add activated_addresses
 
 Revision ID: 1ee76cf990d9
-Revises: 4edfa46798f8
+Revises: 43ed89eaa2cb
 Create Date: 2014-08-19 17:19:17.289870
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1ee76cf990d9'
-down_revision = '4edfa46798f8'
+down_revision = '43ed89eaa2cb'
 
 from alembic import op
 import sqlalchemy as sa
