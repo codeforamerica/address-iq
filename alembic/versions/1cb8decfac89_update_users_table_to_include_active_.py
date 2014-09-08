eabd58900694be9a4cb543acc21ceffbb032037b
Fureigh <info@fureigh.com>
2014-09-07 23:56:32 -0700

    [76827278] Update downrevision in an Alembic version file.

--- alembic/versions/1cb8decfac89_update_users_table_to_include_active_.py
+++ alembic/versions/1cb8decfac89_update_users_table_to_include_active_.py
@@ -1,14 +1,14 @@
 """update users table to include active status and login info
 
 Revision ID: 1cb8decfac89
-Revises: 43ed89eaa2cb
+Revises: 1ee76cf990d9
 Create Date: 2014-08-29 17:15:02.149325
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1cb8decfac89'
-down_revision = '43ed89eaa2cb'
+down_revision = '1ee76cf990d9'
 
 from alembic import op
 import sqlalchemy as sa
