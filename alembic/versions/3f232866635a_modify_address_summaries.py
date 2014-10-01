15c6df041a402ebc595dec596ea5c9ce3721b5ca
Fureigh <info@fureigh.com>
2014-09-30 19:22:45 -0700

    Fix order of alembic versions.

--- alembic/versions/3f232866635a_modify_address_summaries.py
+++ alembic/versions/3f232866635a_modify_address_summaries.py
@@ -1,14 +1,14 @@
 """modify address_summaries
 
 Revision ID: 3f232866635a
-Revises: 1ee76cf990d9
+Revises: 169f2e275465
 Create Date: 2014-09-06 13:26:25.239336
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '3f232866635a'
-down_revision = '1ee76cf990d9'
+down_revision = '169f2e275465'
 
 from alembic import op
 import sqlalchemy as sa
